f84dac96a3c33a999e73ad550bd9c44175218301
Chris Guest <chrisguest75@users.noreply.github.com>
2022-12-18 23:12:15 +0000

    First commit trying out a few things..

---
+++ 01_cmdline/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        string[] files = Directory.GetFiles(".", "*.*", SearchOption.AllDirectories);
+        foreach (string file in files) {
+            Console.WriteLine(file);
+        }
+
+
+    }
+}
